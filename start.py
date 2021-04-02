bd3d5867a876128a1e74133a459f4a6cc83e88ec
unknown <unknown@example.com>
2021-04-03 04:56:59 +0900

    socket

--- start.py
+++ start.py
@@ -1,6 +1,20 @@
 import subprocess
 import webbrowser
+import time
+import socket
 
 
-webbrowser.open("http://127.0.0.1:8000/")
-subprocess.run("python manage.py runserver", shell=True)
+subprocess.Popen("python manage.py runserver", shell=True)
+
+while True:
+    time.sleep(1)
+    try:
+        socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket1.connect(("127.0.0.1", 8000))
+        socket1.close()
+        webbrowser.open("http://127.0.0.1:8000/")
+        break
+    except Exception as e:
+        print(e)
+
+print("スタート！！！")
